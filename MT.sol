cd88d294701f7568ed351b944031762e51fe8534
sefesquivel <jesquivel@gmail.com>
2024-02-27 00:02:09 +0800

    Update MT.sol

--- MT.sol
+++ MT.sol
@@ -8,47 +8,51 @@ contract MQuizContract {
     uint256 public hrsWrk;
     uint256 public rate;
     uint256 public totSal;
+    address public owner;
 
     event SalaryCalculated(string message, string name, uint256 totalSalary);
 
-    function setName(string calldata _name) public {
-        name = _name;
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
     }
 
-    function getName() public view returns (string memory) {
-        return name;
+    modifier validRate(uint256 _rate) {
+        require(_rate > 0, "Rate must be greater than zero");
+        _;
+    }
+
+    modifier validHrsWrk(uint256 _hrsWrk) {
+        require(_hrsWrk > 0, "Hours worked must be greater than zero");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function setName(string calldata _name) public {
+        name = _name;
     }
 
     function setAge(uint256 _age) public {
         age = _age;
     }
 
-    function getAge() public view returns (uint256) {
-        return age;
-    }
-
     function setHrsWrk(uint256 _hrsWrk) public {
         hrsWrk = _hrsWrk;
     }
 
-    function getHrsWrk() public view returns (uint256) {
-        return hrsWrk;
-    }
-
-    function setRate(uint256 _rate) public {
+    function setRate(uint256 _rate) public onlyOwner validRate(_rate) {
         rate = _rate;
     }
 
-    function getRate() public view returns (uint256) {
-        return rate;
-    }
-
-    function calculateTotSal() public { 
-        totSal = rate * hrsWrk;
-        emit SalaryCalculated("Hello, " , name, totSal);
-    }
-
-    function getTotSal() public view returns (uint256) {
-        return totSal;
+    function calculateTotSal() public onlyOwner { 
+        if (rate > 0 && hrsWrk > 0) {
+            totSal = rate * hrsWrk;
+            emit SalaryCalculated("Hello, ", name, totSal);
+        } else {
+            revert("Rate or hours worked is zero");
+        }
     }
 }
