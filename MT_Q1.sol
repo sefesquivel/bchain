891ba7b02066408e325dc6dc87c1b52bf203d56c
sefesquivel <jesquivel@gmail.com>
2024-02-27 13:38:13 +0800

    Create MT_Q1.sol

---
+++ MT_Q1.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MQuizContract {
+    uint256 public age;
+    uint256 public hrsWrk;
+    uint256 public rate;
+    uint256 public totSal;
+    address public owner;
+
+    event SalaryCalculated(string message, uint256 totalSalary);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
+
+    modifier validRate(uint256 _rate) {
+        require(_rate > 0, "Rate must be greater than zero");
+        _;
+    }
+
+    modifier validHrsWrk(uint256 _hrsWrk) {
+        require(_hrsWrk > 0, "Hours worked must be greater than zero");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function setAge(uint256 _age) public {
+        age = _age;
+    }
+
+    function setHrsWrk(uint256 _hrsWrk) public {
+        hrsWrk = _hrsWrk;
+    }
+
+    function setRate(uint256 _rate) public onlyOwner validRate(_rate) {
+        rate = _rate;
+    }
+
+    function calculateTotSal() public onlyOwner { 
+        if (rate > 0 && hrsWrk > 0) {
+            totSal = rate * hrsWrk;
+            emit SalaryCalculated("Hello Joseph ", totSal);
+        } else {
+            revert("Rate or hours worked is zero");
+        }
+    }
+}
