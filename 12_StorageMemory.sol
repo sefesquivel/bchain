f56951a4ff9c4e1cd420ca08b74f1278da8bc665
sefesquivel <jesquivel@gmail.com>
2024-02-27 12:22:15 +0800

    Update and rename 12_SotrageMemory.sol to 12_StorageMemory.sol

--- 12_StorageMemory.sol
+++ 12_StorageMemory.sol
@@ -7,18 +7,15 @@ contract sampStorage
 // Initialising array numbers
 uint256[] public numbers;
 
-// Function to insert values
-// in the array numbers
+// Function to insert values in the array numbers
 function Numbers() public
 {
 	numbers.push(1);
 	numbers.push(2);
-
-	//Creating a new instance
+	//Creating a new instance using storage or memory
 	uint256[] storage myArray = numbers;
 	
-	// Adding value to the 
-	// first index of the new Instance
-	myArray[0] = 10;
+	// Adding value to the first index of the new Instance
+	myArray[0] = 0;
 } 
 }
