bca4ba8a2f58136ed4a11e13b62a24d0eb1e4347
sefesquivel <jesquivel@gmail.com>
2024-02-27 09:04:27 +0800

    Update 10_Struct.sol

--- 10_Struct.sol
+++ 10_Struct.sol
@@ -12,9 +12,11 @@ contract Strucs {
     mapping(address => Car[]) public carsByOwner;
 
     function someApplications() external {
+        
         Car memory toyota = Car("Toyota", 1990, msg.sender);
         Car memory lambo = Car({year:1980, model:"lamborghini", owner:msg.sender });
         Car memory tesla;
+        
         tesla.model = "Tesla";
         tesla.year = 1983;
         tesla.owner = msg.sender;
@@ -28,7 +30,7 @@ contract Strucs {
         Car storage _car = cars[0];
         _car.year = 1990;
         delete _car.owner;
-        
+
         delete cars[1];
     }
 }
