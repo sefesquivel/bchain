07f185be1444ba3ed58dd5297c4a4a33df49d3cd
sefesquivel <jesquivel@gmail.com>
2024-02-27 12:19:41 +0800

    Create 12_SotrageMemory.sol

---
+++ 12_SotrageMemory.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+// Creating a contract
+contract sampStorage
+{
+// Initialising array numbers
+uint256[] public numbers;
+
+// Function to insert values
+// in the array numbers
+function Numbers() public
+{
+	numbers.push(1);
+	numbers.push(2);
+
+	//Creating a new instance
+	uint256[] storage myArray = numbers;
+	
+	// Adding value to the 
+	// first index of the new Instance
+	myArray[0] = 10;
+} 
+}
