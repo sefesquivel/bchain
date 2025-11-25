4c21b394b9f0a22fe8396f7bb4fcb78e8ca18b1d
sefesquivel <jesquivel@gmail.com>
2025-11-25 10:50:32 +0800

    Create Cons_and_Vars.sol

---
+++ Cons_and_Vars.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+
+contract Constants {
+    address public constant MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+    uint public constant MY_UNIT = 123;
+}
+
+contract Var {
+    address public MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+}
